726fc4b529afe922f60731d37c708243760ae4e0
IamJayB <134087001+jaysreeb@users.noreply.github.com>
2026-08-25 20:06:46 +0530

    Add: is-anagram - submission-17

---
+++ Data Structures & Algorithms/is-anagram/submission-17.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_count, t_count = {}, {}
+
+        if len(s) != len(t):
+            return False
+
+        for char in s:
+            s_count[char] = s_count.get(char, 0) + 1
+        for char in t:
+            t_count[char] = t_count.get(char, 0) + 1
+        
+        return s_count == t_count
+            
